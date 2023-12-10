b16b9d3235aa59d4af3fb1a09ac5c3a2fdb15bea
Raluca Streja <raluca.streja@gmail.com>
2023-12-10 14:20:54 +0200

    added logout

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
+$_SESSION = array();
 session_destroy();
 // Redirectare paginaprincipala produse:
-header('Location: magazin.php');
-?>
+header('Location: login/login.html');
